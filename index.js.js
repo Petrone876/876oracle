e4846bcc2c1db6f999cc4353465b70d23bb07c01
Petrone876 <53618555+Petrone876@users.noreply.github.com>
2019-09-01 09:35:40 -0700

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,17 @@
+var nature = document.getElementById("open")
+
+function onOpenAction () {
+    var openAction = "http://4.bp.blogspot.com/-SA2-s7Agmzs/TqYihUrJkkI/AAAAAAAAARQ/guUT1Pp0gwQ/s400/caterpillar-6.jpg";
+
+    var imgTarget = document.getElementById (dispImage);
+
+    imgTarget.src = openAction;
+    }
+nature.addEventListener ("click,onOpenAction");
+
+
+
+
+
+
+
