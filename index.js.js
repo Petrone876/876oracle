4045d675426188c69838ffad66e9a8ff867892bc
Patronica <petronecosmetics@gmail.com>
2019-09-12 10:38:15 -0700

    commiting updated JS

--- index.js.js
+++ index.js.js
@@ -1,13 +1,58 @@
-var nature = document.getElementById("open")
+var nature = document.getElementById("open");
 
 function onOpenAction () {
-    var openAction = "http://4.bp.blogspot.com/-SA2-s7Agmzs/TqYihUrJkkI/AAAAAAAAARQ/guUT1Pp0gwQ/s400/caterpillar-6.jpg";
+    var openAction = "http://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/invertebrates/m/monarch_butterfly_thumb.ngsversion.1497024022038.adapt.1900.1.JPG";
 
-    var imgTarget = document.getElementById (dispImage);
+    var imgTarget = document.getElementById ("dispImage");
 
     imgTarget.src = openAction;
     }
-nature.addEventListener ("click,onOpenAction");
+nature.addEventListener ("click",onOpenAction);
+
+
+
+var wood = document.getElementById ("plantGrows")
+
+  function ongrowAction() {
+      var growAction = "https://tse3.mm.bing.net/th?id=OIP.l9IqweMr6a0e4JHyJD5FuAHaEz&pid=Api&P=0&w=255&h=166";
+  
+  var plantImage = document.getElementById ("plantImage");
+
+  plantImage.src = growAction;
+  }
+wood.addEventListener("click", ongrowAction)
+
+
+
+var water = document.getElementById ("waterFalls");
+
+function onWaterAction() {
+  var waterAction = "https://tse3.mm.bing.net/th?id=OIP.1GMs77TEhRbIhLvNTdHHzgHaEo&pid=Api&P=0&w=305&h=191";
+
+  var waterImage = document.getElementById ("waterImage");
+
+  waterImage.src = waterAction;
+  }
+water.addEventListener("click",onWaterAction);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
